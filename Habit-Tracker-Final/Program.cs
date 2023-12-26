0ad5037aa162b88059412eeb112b2598fc908b0f
DLee211 <daniel.lee1921@gmail.com>
2023-12-25 19:57:24 -0500

    Created sqlite database
    Starting project

---
+++ Habit-Tracker-Final/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.VisualBasic;
+
+namespace habit_tracker
+{
+    class Program
+    {
+        private static string connectionString = @"Data Source = habit-tracker.db";
+
+        static void Main(string[] args)
+        {
+            using (var connection = new SqliteConnection(connectionString))
+            {
+                connection.Open();
+
+                connection.Close();
+            }
+        }
+    }
+}

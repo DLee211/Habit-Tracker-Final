70609cb8c894bdd869a4f89061c9e21c56bf11a6
DLee211 <daniel.lee1921@gmail.com>
2023-12-25 20:02:40 -0500

    Created table
    Id, date, and quantity added to table drinking_water

--- Habit-Tracker-Final/Program.cs
+++ Habit-Tracker-Final/Program.cs
@@ -13,6 +13,15 @@ namespace habit_tracker
             {
                 connection.Open();
 
+                var tableCmd = connection.CreateCommand();
+                
+                tableCmd.CommandText = @"CREATE TABLE IF NOT EXISTS drinking_water (
+                                        Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                        Date TEXT, 
+                                        Quantity INTEGER)";
+
+                tableCmd.ExecuteNonQuery();
+
                 connection.Close();
             }
         }

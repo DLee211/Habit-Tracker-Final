0cecfb2943be2efd8614c69a27f991a60c25068a
DLee211 <daniel.lee1921@gmail.com>
2023-12-25 21:32:57 -0500

    updated engine.cs

--- Habit-Tracker-Final/Engine.cs
+++ Habit-Tracker-Final/Engine.cs
@@ -6,36 +6,53 @@ public class Engine
 {
     private static string connectionString = @"Data Source = habit-tracker.db";
 
+    public static bool closeApp = false;
+
     public static void GetUserInput()
     {
-        Console.WriteLine("\nMenu");
-        Console.WriteLine("\nWhat do you want to do?");
-        Console.WriteLine("\ntype 0 if you want to close application");
-        Console.WriteLine("Type 1 to View records");
-        Console.WriteLine("Type 2 to insert record");
-        Console.WriteLine("Type 3 to delete record");
-        Console.WriteLine("Type 4 to update record");
-        Console.WriteLine("--------------------------------------------");
-
-        string command = Console.ReadLine();
-
-        switch (command)
+        while (closeApp == false)
         {
-            case "0":
-                Environment.Exit(0);
-                break;
-            // case "1":
-            //     ViewRecords();
-            //     break;
-            case "2":
-                InsertRecords();
-                break;
-             case "3":
-                 DeleteRecords();
-                 break;
-            // case "4":
-            //     UpdateRecords();
-            //     break;
+            Console.WriteLine("\nMenu");
+            Console.WriteLine("\nWhat do you want to do?");
+            Console.WriteLine("\ntype 0 if you want to close application");
+            Console.WriteLine("Type 1 to View records");
+            Console.WriteLine("Type 2 to insert record");
+            Console.WriteLine("Type 3 to delete record");
+            Console.WriteLine("Type 4 to update record");
+            Console.WriteLine("--------------------------------------------");
+
+            string command = Console.ReadLine();
+
+            switch (command)
+            {
+                case "0":
+                    closeApp = true;
+                    Environment.Exit(0);
+                    break;
+                 case "1":
+                     ViewRecords();
+                     break;
+                case "2":
+                    InsertRecords();
+                    break;
+                case "3":
+                    DeleteRecords();
+                    break;
+                // case "4":
+                //     UpdateRecords();
+                //     break;
+            }
+        }
+    }
+
+    private static void ViewRecords()
+    {
+        string query = "SELECT * FROM drinking_water";
+        using (var connection = new SqliteConnection(connectionString))
+        {
+            connection.Open();
+            
+            
         }
     }
 
